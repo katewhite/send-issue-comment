63537c80d5a3775493020283ee14539d010b3827
Kate White <kate@customer.io>
2019-08-01 11:50:38 -0600

    HS token call

--- index.js
+++ index.js
@@ -9,27 +9,53 @@ Toolkit.run( async ( tools ) => {
     const { title, labels } = issue;
     const { login } = sender;
 
-    var labelNames = '';
-    for (var i = 0; i < labels.length; i++) { 
+    let labelNames = '';
+    for (let i = 0; i < labels.length; i++) { 
       labelNames += labels[i].name + ";";
     }
 
+    let threadsContent = null;
     if (body.includes('+1') && labelNames.includes('feature request')) {
       let ticketNumber = null;
       // Get and format HS ticket threads if a link exists
       if (body.includes('secure.helpscout.net')) {
         tools.log('contains hs link');
-        let pattern = /(?<url>https:\/\/secure.helpscout.net.+\/(?<id>.*)\?.*)/;
+        let pattern = /(?<url>https:\/\/secure.helpscout.net\/conversation\/(?<id>.*)\/.*)/;
         let { groups: { url, id } } = body.match(pattern);
         tools.log('URL and ID:');
         tools.log(url);
         tools.log(id);
 
-        // Get HS ticket threads
-        var getTicketThreads = new Promise( async( resolve, reject ) => {
+        // Get HS access token
+        let getHSToken = new Promise( async( resolve, reject ) => {
           try {
-            var request = new XMLHttpRequest();
-            var url = `https://api.helpscout.net/v2/conversations/${ id }/threads`;
+            let request = new XMLHttpRequest();
+            let url = `https://api.helpscout.net/v2/oauth2/token`;
+
+            request.open('POST', url, true);
+
+            // Send request
+            request.send("grant_type=client_credentials&client_id=0def26e02fe841fbbbe1dff415284eb8&client_secret=da7db41c06814699a0f8f1c9354aa57c");
+
+            resolve();
+          }
+          catch( error ){
+            reject( error );
+          }
+        });
+
+        // Wait for completion
+        getHSToken.then(function(result) {
+          tools.log(result);
+        }, function(err) {
+          tools.exit.failure( err );
+        });
+
+        // Get HS ticket threads
+        let getTicketThreads = new Promise( async( resolve, reject ) => {
+          try {
+            let request = new XMLHttpRequest();
+            let url = `https://api.helpscout.net/v2/conversations/${ id }/threads`;
 
             request.open('GET', url, true);
 
@@ -45,19 +71,29 @@ Toolkit.run( async ( tools ) => {
 
         // Wait for completion
         getTicketThreads.then(function(result) {
-          tools.log.success(
-            `Got threads for ticket '${ ticketNumber }'`
+          tools.log(
+            `Got threads for ticket '${ id }'`
           );
-          tools.log.success(result);
+          tools.log(result);
+
+          // Build the threads HTML
+          threadsContent = '';
+          let { threads } = result;
+          for (let j = 0; j < threads.length; j++) { 
+            threadsContent += 
+            '<hr><strong>From: </strong>' + threads[j].createdBy.email +
+            '<p>' + threads[j].body + '</p>';
+          }
+
         }, function(err) {
           tools.exit.failure( err );
         });
       }
 
       // Send to Zapier
-      var sendComment = new Promise( async( resolve, reject ) => {
+      let sendComment = new Promise( async( resolve, reject ) => {
         try {
-          var request = new XMLHttpRequest();
+          let request = new XMLHttpRequest();
 
           request.open('POST', 'https://hooks.zapier.com/hooks/catch/3679287/oon4u7z/', true);
 
@@ -88,7 +124,7 @@ Toolkit.run( async ( tools ) => {
         tools.exit.failure( err );
       });
     } else {
-      var labelString = labels.toString();
+      let labelString = labels.toString();
       tools.exit.neutral( `New comment for '${ issue.title }' does not contain '+1' or the issue is not a feature request. Comment: '${ body }' -- Labels: ${ labelNames }'` );
     }
   }
@@ -99,3 +135,17 @@ Toolkit.run( async ( tools ) => {
   event: [ 'issue_comment' ],
   secrets: [ 'GITHUB_TOKEN' ],
 })
+
+
+
+ID: 0def26e02fe841fbbbe1dff415284eb8
+Secret: da7db41c06814699a0f8f1c9354aa57c
+
+curl -X POST https://api.helpscout.net/v2/oauth2/token \
+    --data "grant_type=client_credentials" \
+    --data "client_id=0def26e02fe841fbbbe1dff415284eb8" \
+    --data "client_secret=da7db41c06814699a0f8f1c9354aa57c"
+
+curl -X GET https://api.helpscout.net/v2/conversations/85065 -H "Authorization: Bearer 806c10f97cb6435eab6cdaf8c973246c"
+
+
