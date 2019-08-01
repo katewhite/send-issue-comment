6c11d8a8ec3900ce4fd669ee55c42d0961af4023
Kate White <kate@customer.io>
2019-08-01 08:16:35 -0600

    Check for +1 and feature request label

--- index.js
+++ index.js
@@ -6,44 +6,47 @@ Toolkit.run( async ( tools ) => {
   try {
     const { comment, issue, sender } = tools.context.payload;
     const { body, html_url, created_at } = comment;
-    const { title } = issue;
+    const { title, labels } = issue;
     const { login } = sender;
 
     
+    if (body.includes('+1') && labels.includes('feature request')) {
+      var sendComment = new Promise( async( resolve, reject ) => {
+        try {
+          var request = new XMLHttpRequest();
 
-    var sendComment = new Promise( async( resolve, reject ) => {
-      try {
-        var request = new XMLHttpRequest();
+          request.open('POST', 'https://hooks.zapier.com/hooks/catch/3679287/oon4u7z/', true);
 
-        request.open('POST', 'https://hooks.zapier.com/hooks/catch/3679287/oon4u7z/', true);
+          const data = {
+            body,
+            html_url,
+            created_at,
+            title,
+            login
+          }
 
-        const data = {
-          body,
-          html_url,
-          created_at,
-          title,
-          login
+          // Send request
+          request.send(JSON.stringify(data));
+
+          resolve();
         }
+        catch( error ){
+          reject( error );
+        }
+      });
 
-        // Send request
-        request.send(JSON.stringify(data));
-
-        resolve();
-      }
-      catch( error ){
-        reject( error );
-      }
-    });
-
-    // Wait for completion
-    sendComment.then(function(result) {
-      tools.log.success(
-        `Sent new issue comment for ${ issue.title } to Zapier.`
-      );
-      tools.log.success(result);
-    }, function(err) {
-      tools.exit.failure( err );
-    });
+      // Wait for completion
+      sendComment.then(function(result) {
+        tools.log.success(
+          `Sent new issue comment for '${ issue.title }' to Zapier.`
+        );
+      }, function(err) {
+        tools.exit.failure( err );
+      });
+    } else {
+      var labelString = labels.toString();
+      tools.exit.neutral( `New comment for '${ issue.title }' does not contain '+1' or the issue is not a feature request. Comment: '${ body }' -- Labels: ${ labelString }'` );
+    }
 
 
     // if( action !== 'assigned' ){
