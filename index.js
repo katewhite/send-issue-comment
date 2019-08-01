f0aa865bd132f9ab9959577f86db372e64222ca8
Kate White <kate@customer.io>
2019-08-01 15:45:12 -0600

    debug

--- index.js
+++ index.js
@@ -19,141 +19,95 @@ Toolkit.run( async ( tools ) => {
     if (body.includes('+1') && labelNames.includes('feature request')) {
       let ticketNumber = null;
       let hsToken = null;
+      
+      let sendComment = function(response) {
+        return new Promise((resolve, reject) => {
+          // Build the threads HTML
+          threadsContent = '';
+          let threads = result._embedded.threads;
+          let customerName = `${ result._embedded.threads[0].customer.first } ${ result._embedded.threads[0].customer.last }`;
+          let customerEmail = result._embedded.threads[0].customer.email;
+          for (let j = 0; j < threads.length; j++) { 
+            threadsContent += 
+            '<br><hr><br><strong>From: </strong>' + threads[j].createdBy.email +
+            '<p>' + threads[j].body + '</p>';
+          }
+
+          // Send comment
+          axios.post('https://hooks.zapier.com/hooks/catch/3679287/oon4u7z/', {
+            body,
+            html_url,
+            created_at,
+            title,
+            login, 
+            threadsContent,
+            customerName,
+            customerEmail
+          })
+          .then(function (response) {
+            resolve();
+          })
+          .catch(function (error) {
+            reject(error);
+          });
+        });
+      };
+
       // Get and format HS ticket threads if a link exists
       if (body.includes('secure.helpscout.net')) {
         tools.log('contains hs link');
         let pattern = /(?<url>https:\/\/secure.helpscout.net\/conversation\/(?<id>.*)\/.*)/;
         let { groups: { url, id } } = body.match(pattern);
 
-        // Get HS access token
-        axios.post('https://api.helpscout.net/v2/oauth2/token', {
-          grant_type: "client_credentials", 
-          client_id: "0def26e02fe841fbbbe1dff415284eb8",
-          client_secret: "da7db41c06814699a0f8f1c9354aa57c"
-        })
-        .then(function (response) {
-          tools.log(response);
-        })
-        .catch(function (error) {
-          tools.log(error);
+        let getToken = function(response) {
+          return new Promise((resolve, reject) => {
+            // Get HS access token
+            axios.post('https://api.helpscout.net/v2/oauth2/token', {
+              grant_type: "client_credentials", 
+              client_id: "0def26e02fe841fbbbe1dff415284eb8",
+              client_secret: "da7db41c06814699a0f8f1c9354aa57c"
+            })
+            .then(function (response) {
+              tools.log(response);
+              resolve(response);
+            })
+            .catch(function (error) {
+              reject(error);
+            });
+          });
+        };
+
+        let getTicketThreads = function(response) {
+          return new Promise((resolve, reject) => {
+            // Get ticket threads
+            let url = `https://api.helpscout.net/v2/conversations/${ id }/threads`
+            axios.get(url)
+            .then(function (response) {
+              let response = JSON.parse(this.responseText);
+              resolve(response);
+            })
+            .catch(function (error) {
+              reject(error);
+            });
+          });
+        };
+        
+        getToken().then(getTicketThreads).then(sendComment).then(function(result) {
+          tools.log.success(
+            `Sent new issue comment for '${ issue.title }' to Zapier.`
+          );
+        }, function(err) {
+          tools.exit.failure( err );
+        });;
+      } else {
+        sendComment.then(function(result) {
+          tools.log.success(
+            `Sent new issue comment for '${ issue.title }' to Zapier.`
+          );
+        }, function(err) {
+          tools.exit.failure( err );
         });
-          // let getHSToken = new Promise( async( resolve, reject ) => {
-          //   try {
-          //     let request = new XMLHttpRequest();
-          //     let url = `https://api.helpscout.net/v2/oauth2/token`;
-          //     var data = new FormData();
-          //     data.append("grant_type", "client_credentials");
-          //     data.append("client_id", "0def26e02fe841fbbbe1dff415284eb8");
-          //     data.append("client_secret", "da7db41c06814699a0f8f1c9354aa57c");
-
-          //     request.addEventListener("readystatechange", function () {
-          //       tools.log('readystatechange');
-          //       tools.log(this.readyState);
-          //       if (this.readyState === 4) {
-          //         tools.log('RESPONSE TEXT:');
-          //         tools.log(this.responseText);
-          //         resolve(request.responseText);
-          //       }
-          //     });
-
-          //     request.open('POST', url, true);
-          //     request.setRequestHeader('Content-type', 'multipart/form-data');
-
-          //     // Send request
-          //     request.send(data);
-          //   }
-          //   catch( error ){
-          //     reject( error );
-          //   }
-          // });
-
-          // // Wait for completion
-          // getHSToken.then(function(result) {
-          //   tools.log('TOKEN:');
-          //   tools.log(result);
-          //   hsToken = result;
-          // });
-
-          // Get HS ticket threads
-          let getTicketThreads = new Promise( async( resolve, reject ) => {
-            try {
-              let request = new XMLHttpRequest();
-              let url = `https://api.helpscout.net/v2/conversations/${ id }/threads`;
-
-              request.addEventListener("readystatechange", function () {
-                if (this.readyState === 4) {
-                  let response = JSON.parse(this.responseText);
-                  resolve(response);
-                }
-              });
-
-              request.open('GET', url, true);
-              request.setRequestHeader("Authorization", "Bearer b43fe5f8441b4deb969422a827c1822b");
-
-              // Send request
-              request.send();
-            }
-            catch( error ){
-              reject( error );
-            }
-          });
-
-          // Wait for completion
-          getTicketThreads.then(function(result) {
-            tools.log(`Got threads for ticket '${ id }'`);
-
-            // Build the threads HTML
-            threadsContent = '';
-            let threads = result._embedded.threads;
-            let customerName = `${ result._embedded.threads[0].customer.first } ${ result._embedded.threads[0].customer.last }`;
-            let customerEmail = result._embedded.threads[0].customer.email;
-            for (let j = 0; j < threads.length; j++) { 
-              threadsContent += 
-              '<br><hr><br><strong>From: </strong>' + threads[j].createdBy.email +
-              '<p>' + threads[j].body + '</p>';
-            }
-
-            // Send to Zapier
-            let sendComment = new Promise( async( resolve, reject ) => {
-              try {
-                let request = new XMLHttpRequest();
-
-                request.open('POST', 'https://hooks.zapier.com/hooks/catch/3679287/oon4u7z/', true);
-
-                const data = {
-                  body,
-                  html_url,
-                  created_at,
-                  title,
-                  login, 
-                  threadsContent,
-                  customerName,
-                  customerEmail
-                }
-
-                // Send request
-                request.send(JSON.stringify(data));
-
-                resolve();
-              }
-              catch( error ){
-                reject( error );
-              }
-            });
-
-            // Wait for completion
-            sendComment.then(function(result) {
-              tools.log.success(
-                `Sent new issue comment for '${ issue.title }' to Zapier.`
-              );
-            }, function(err) {
-              tools.exit.failure( err );
-            });
-
-          }, function(err) {
-            tools.exit.failure( err );
-          });
-      }
+      } 
     } else {
       let labelString = labels.toString();
       tools.exit.neutral( `New comment for '${ issue.title }' does not contain '+1' or the issue is not a feature request. Comment: '${ body }' -- Labels: ${ labelNames }'` );
@@ -169,6 +123,7 @@ Toolkit.run( async ( tools ) => {
 
 
 
+
 // ID: 0def26e02fe841fbbbe1dff415284eb8
 // Secret: da7db41c06814699a0f8f1c9354aa57c
 
@@ -179,4 +134,85 @@ Toolkit.run( async ( tools ) => {
 
 // curl -X GET https://api.helpscout.net/v2/conversations/85065 -H "Authorization: Bearer 806c10f97cb6435eab6cdaf8c973246c"
 
+
+
+// Get HS ticket threads
+          // let getTicketThreads = new Promise( async( resolve, reject ) => {
+          //   try {
+          //     let request = new XMLHttpRequest();
+          //     let url = `https://api.helpscout.net/v2/conversations/${ id }/threads`;
+
+          //     request.addEventListener("readystatechange", function () {
+          //       if (this.readyState === 4) {
+          //         let response = JSON.parse(this.responseText);
+          //         resolve(response);
+          //       }
+          //     });
+
+          //     request.open('GET', url, true);
+          //     request.setRequestHeader("Authorization", "Bearer b43fe5f8441b4deb969422a827c1822b");
+
+          //     // Send request
+          //     request.send();
+          //   }
+          //   catch( error ){
+          //     reject( error );
+          //   }
+          // });
+
+          // Wait for completion
+          // getTicketThreads.then(function(result) {
+          //   tools.log(`Got threads for ticket '${ id }'`);
+
+          //   // Build the threads HTML
+          //   threadsContent = '';
+          //   let threads = result._embedded.threads;
+          //   let customerName = `${ result._embedded.threads[0].customer.first } ${ result._embedded.threads[0].customer.last }`;
+          //   let customerEmail = result._embedded.threads[0].customer.email;
+          //   for (let j = 0; j < threads.length; j++) { 
+          //     threadsContent += 
+          //     '<br><hr><br><strong>From: </strong>' + threads[j].createdBy.email +
+          //     '<p>' + threads[j].body + '</p>';
+          //   }
+
+          //   // Send to Zapier
+          //   let sendComment = new Promise( async( resolve, reject ) => {
+          //     try {
+          //       let request = new XMLHttpRequest();
+
+          //       request.open('POST', 'https://hooks.zapier.com/hooks/catch/3679287/oon4u7z/', true);
+
+          //       const data = {
+          //         body,
+          //         html_url,
+          //         created_at,
+          //         title,
+          //         login, 
+          //         threadsContent,
+          //         customerName,
+          //         customerEmail
+          //       }
+
+          //       // Send request
+          //       request.send(JSON.stringify(data));
+
+          //       resolve();
+          //     }
+          //     catch( error ){
+          //       reject( error );
+          //     }
+          //   });
+
+            // Wait for completion
+          //   sendComment.then(function(result) {
+          //     tools.log.success(
+          //       `Sent new issue comment for '${ issue.title }' to Zapier.`
+          //     );
+          //   }, function(err) {
+          //     tools.exit.failure( err );
+          //   });
+
+          // }, function(err) {
+          //   tools.exit.failure( err );
+          // });
 
